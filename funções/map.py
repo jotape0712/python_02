5aee33d960cc82a4d275c746f5a5794b24d958f4
Joao Pedro Bezerra <joaopedrobezerradev@gmail.com>
2025-04-17 13:16:10 -0300

    ...

--- funções/map.py
+++ funções/map.py
@@ -1,8 +1,10 @@
-
-notas = [6, 9.3, 7.7] 
+notas = [6, 9.3, 7.7]  
 qualitativo = 0.5
 
+# Utiliza a função `map` para aplicar a função lambda em cada elemento da lista `notas`
 media_atualizada = map(lambda x: x + qualitativo, notas)
+
+# Converte o objeto `map` para uma lista
 media_atualizada = list(media_atualizada)
 
-print(media_atualizada)      
+print(media_atualizada)
